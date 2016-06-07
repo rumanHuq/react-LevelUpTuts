7aa04a695e0b97c9c93bd46b2e7444071af8a2a3
rumanbsl <rumanbsl@gmail.com>
2016-06-07 14:16:39 +0300

    form field workable

--- src/ContactList.js
+++ src/ContactList.js
@@ -3,16 +3,29 @@ import {render} from 'react-dom';
 import Contact from './Contact';
 
 class ContactList extends React.Component {
+    constructor (){
+        super();
+        this.state = {
+            search : ''
+        }
+    }
+    _onChange(event){
+        this.setState({search: event.target.value.substr(0,5)})
+    }
     render(){
         return (
             <div>
                 {this.props.contacts.map((contact)=>{
                     return <Contact contact={contact} key={contact.id}/>
                 })}
-
+                <input type="text"
+                       value={this.state.search}
+                       onChange = {this._onChange.bind(this)}
+                       />
             </div>
         );
     }
 }
 
 export default ContactList;
+
